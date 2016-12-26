de0f374ab24f5cefb1448876b29554cc499c7e63
Patrick Lam <prof.lam@gmail.com>
2016-12-25 23:10:51 -0800

    better styling, show UWids, drop bootstrap

--- lib/student_detail_component.dart
+++ lib/student_detail_component.dart
@@ -14,6 +14,7 @@ import 'student_service.dart';
 
 @Component(
   selector: 'my-student-detail',
+  styleUrls: const ['student_detail_component.css'],
   templateUrl: 'student_detail_component.html',
   )
 class StudentDetailComponent implements OnInit {
