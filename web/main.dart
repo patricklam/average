105fc0e0313a6355979541256a95029d216a95b1
Patrick Lam <prof.lam@gmail.com>
2016-12-27 20:55:55 -0800

    update deps, provide BrowserClient

--- web/main.dart
+++ web/main.dart
@@ -1,17 +1,18 @@
 // Copyright (c) 2016, Patrick Lam. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'package:http/browser_client.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/platform/common.dart';
 import 'package:angular2/angular2.dart';
 import 'package:angular2/router.dart';
+import 'package:http/http.dart';
 
 import 'package:average/app_component.dart';
 import 'package:average/students_component.dart';
 
 main() {
   bootstrap(AppComponent,
-    [ROUTER_PROVIDERS,
-     const Provider(LocationStrategy, useClass:HashLocationStrategy)
+    [provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
     ]);
 }
