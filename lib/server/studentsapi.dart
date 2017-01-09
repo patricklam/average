e887e78b50c8d61e38077cb894ea7cd305e40c18
Patrick Lam <prof.lam@gmail.com>
2017-01-08 22:06:08 -0500

    add more API methods to manipulate Students

--- lib/server/studentsapi.dart
+++ lib/server/studentsapi.dart
@@ -13,7 +13,7 @@ import '../common/course_mark.dart';
 
 @ApiClass(version: 'v1')
 class StudentsApi {
-  final Map<int, Student> _students = {};
+  final Map<int, Student> _students = {}; int _studentsMaxID = 0;
   final Map<int, CourseMark> _courseMarks = {};
   final Map<int, StudentEnrolment> _studentEnrolments = {};
 
@@ -26,6 +26,12 @@ class StudentsApi {
     return _students.values.toList();
   }
 
+  // Returns a student
+  @ApiMethod(method: 'GET', path: 'student/{internal_id}')
+  Student getStudent(String internal_id) {
+    return _students[int.parse(Uri.decodeComponent(internal_id))];
+  }
+
   // Returns a list of course marks
   @ApiMethod(method: 'GET', path: 'course_marks')
   List<CourseMark> listCourseMarks() {
@@ -36,5 +42,18 @@ class StudentsApi {
   @ApiMethod(method: 'GET', path: 'student_enrolments')
   List<StudentEnrolment> listStudentEnrolments() {
     return _studentEnrolments.values.toList();
+  }
+
+  @ApiMethod(method: 'POST', path: 'student')
+  Student addStudent(Student ns) {
+    var newStudent = new Student.fromArgs(_makeFreshID(), ns.uwid, ns.firstname, ns.lastname);
+    _students[newStudent.internal_id] = newStudent;
+    return newStudent;
+  }
+
+  int _makeFreshID() {
+    while (_students.containsKey(_studentsMaxID))
+      _studentsMaxID++;
+    return _studentsMaxID;
   }
 }
