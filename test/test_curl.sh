b899d1b53829476b66e0115edb858e8b14e9764b
Patrick Lam <prof.lam@gmail.com>
2017-01-09 18:36:23 -0500

    add test script

--- test/test_curl.sh
+++ test/test_curl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl -H "Content-Type: application/json" -d '{"uwid":"99119924", "firstname": "r12", "lastname": "ttt"}' http://localhost:8088/studentsApi/v1/student
-curl -H "Content-Type: application/json" -d '{"uwid":"99119923", "firstname": "q11", "lastname": "rss"}' http://localhost:8088/studentsApi/v1/student
+curl -H "Content-Type: application/json" -d '{"internal_id":-1, "uwid":"99119924", "firstname": "r12", "lastname": "ttt"}' http://localhost:8088/studentsApi/v1/student
+curl -H "Content-Type: application/json" -d '{"internal_id":-1, "uwid":"99119923", "firstname": "q11", "lastname": "rss"}' http://localhost:8088/studentsApi/v1/student
 curl -H "Content-Type: application/json" -d '{"student_internal_id":"0", "course": "ECE", "section": "105", "mark": "88"}' http://localhost:8088/studentsApi/v1/course_mark
 curl -H "Content-Type: application/json" -d '{"student_internal_id":"1", "course": "ECE", "section": "105", "mark": "77"}' http://localhost:8088/studentsApi/v1/course_mark
 curl -H "Content-Type: application/json" -d '{"student_internal_id":"0", "term": "1169", "program": "SE", "level": "1A"}' http://localhost:8088/studentsApi/v1/student_enrolment
